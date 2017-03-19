d53e7b2e792f8e629bd85debc220264b6add4673
patriciochavez <patricio_chavez@hotmail.com>
2017-03-19 16:40:23 -0300

    ws support added

--- index.js
+++ index.js
@@ -2,30 +2,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-//var WebSocketServer = require('ws').Server;
+var WebSocketServer = require('ws').Server;
 //var sesion_estado = "NULA";
 //var timer;
 var http = require('http');
 
-//var location = new Object();
+var location = new Object();
 
 var httpServer = http.createServer(app).listen(8080);
-//var wss = new WebSocketServer({server: httpServer});
+var wss = new WebSocketServer({server: httpServer});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
         extended: true
         }));
 
-/*
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
-    html_player_controller = JSON.parse(message);
-    if(html_player_controller.sesion == "NUEVA_SESION") {
-    sesion_estado = "ACTIVA";
-    wss.broadcast(JSON.stringify(html_player_controller));
-    console.log(JSON.stringify(html_player_controller));
-    timer = setTimeout(function(){ 
+    location = JSON.parse(message);
+    wss.broadcast(JSON.stringify(location));
+    console.log(JSON.stringify(location));
+    /*timer = setTimeout(function(){ 
         sesion_estado = "NULA"; 
                 html_player_controller.sesion= "FINALIZAR_SESION";
         console.log(JSON.stringify(html_player_controller));
@@ -43,7 +40,7 @@ wss.on('connection', function(ws) {
     var objeto = new Object();
     objeto = JSON.parse(message);
     wss.broadcast(JSON.stringify(objeto));
-        //wss.broadcast(message);
+        //wss.broadcast(message);*/
     } 
     });
 });
@@ -53,13 +50,15 @@ wss.broadcast = function(data) {
         this.clients[i].send(data);
     }
 };
-*/
 
 app.get(/^(.+)$/, function(req, res){ 
     switch(req.params[0]) {
         case '/test':
             res.send("Ok!");
             break;
+        case '/location':
+            res.send(JSON.stringify(location));
+            break;            
     default: //res.sendFile( __dirname + req.params[0]); 
     }
  });
